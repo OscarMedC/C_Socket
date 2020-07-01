3557380b64053c4bf407797f8cbf6cb988ce4cd3
Oscar Olsson <pinnenolsson@hotmail.com>
2020-07-01 20:07:38 +0200

    server

--- main.c
+++ main.c
@@ -7,62 +7,64 @@
 int main(int argc, char* argv[])
 {
 	WSADATA wsa;
-	SOCKET s;
-	struct sockaddr_in server;
-	char* message, server_reply[2000];
-	int recv_size;
+	int retCode = 0;
 
-	printf(" \nInitialising Winsock...  ");
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-	{
-		printf(" Failed.Error Code : % d " , WSAGetLastError());
-		return 1;
+	// Initierar Winsock Version 2.2
+	retCode = WSAStartup(MAKEWORD(2, 2), &wsa);
+
+	if (retCode != 0) {
+		printf("WSAStartup failed to initialize, error %d\n", retCode);
+	}
+	else {
+		printf("The Winsock .dll was found!\n");
+		printf("Current status: %s.\n", wsa.szSystemStatus);
 	}
 
-	printf("Initialised.\n");
+	// Create a socket called m_socket;
+	SOCKET m_socket;
 
-	//Create a socket
-	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
-	{
-		printf("Could not create socket : % d ", WSAGetLastError());
+	// Call the socket function and return its value to the m_socket variable
+	// Using Internet address family, streaming sockets, and the TCP/IP protocol.
+	// Using AF_INET family, TCP socket type and protocol of the AF_INET - IPv4
+	m_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	// Check for errors to ensure that the socket is a valid socket
+	if (m_socket == INVALID_SOCKET) {
+		printf("Error at m_socket: %ld\n", WSAGetLastError());
 	}
+	else
+		printf("m_socket is OK!\n");
 
-	printf("Socket created.\n ");
+	// bind()
+	// Create a sockaddr_in object and set its values.
+	SOCKADDR_IN service;
 
+	// AF_INET is the Internet address family.
+	service.sin_family = AF_INET;
+	// "127.0.0.1" is the local IP address to which the socket will be bound.
+	service.sin_addr.s_addr = inet_addr("127.0.0.1");
+	// 55555 is the port number to wich the socket will be bound.
+	service.sin_port = htons(55555);
 
-	server.sin_addr.s_addr = inet_addr("194.15.212.162");
-	server.sin_family = AF_INET;
-	server.sin_port = htons(80);
-
-	//Connect to remote server
-	if (connect(s, (struct sockaddr*)&server, sizeof(server)) < 0)
-	{
-		puts("connect error");
-		return 1;
+	// Call the bind function, passing the created socket and the sockaddr_in structure as parameters.
+	// Check for general errors.
+	if (bind(m_socket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
+		printf("bind() failed, %ld.\n", WSAGetLastError());
 	}
+	else
+		printf("bind() was successfull!\n");
 
-	puts("Connected");
+	// Call the listen function, passing the created socket and the maximum number of
+	// allowed connections to accept as parameters. Check for general errors.
 
-	//Send some data
-	message = "GET / HTTP / 1.1\r\n\r\n ";
-	if (send(s, message, strlen(message), 0) < 0)
-	{
-		puts("Send failed");
-		return 1;
+	if (listen(m_socket, 1) == SOCKET_ERROR) {
+		printf("listen() failed, %ld.\n", WSAGetLastError());
 	}
-	puts("Data Send\n");
-
-	//Receive a reply from the server
-	if ((recv_size = recv(s, server_reply, 2000, 0)) == SOCKET_ERROR)
-	{
-		puts("recv failed");
-	}
-
-	puts("Reply received\n");
-
-	//Add a NULL terminating character to make it a proper string before printing
-	server_reply[recv_size] = '\0';
-	puts(server_reply);
+	else
+		printf("listen() is OK, waiting for connections...\n");
+	
+	closesocket(m_socket);
+	WSACleanup();
 
 	return 0;
 }
